4ec1984d871c712803105be901c292b2170340a8
robson1988 <robert.bajoo@gmail.com>
2018-09-28 18:48:08 +0200

    #

---
+++ Models/admin.php
@@ -0,0 +1,1 @@
+<?php include_once "../public_html/Content/includes/header_admin.php";?>
---
+++ public_html/Content/includes/header_admin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="../public_html/Content/includes/style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-fluid p-0">
+<div class="top-navigation">
+  <ul class="nav justify-content-end p-0 bg-dark">
+    <li class= "nav-item"><a class="nav-link" href="../public_html/index.php"><i class="fas fa-home"></i> Go to website</a></li>
+    <li class= "nav-item"><a class="nav-link" href="#"><i class="fas fa-envelope"></i> Messeges</a></li>
+    <li class= "nav-item"><a class="nav-link" href="#"><i class="fas fa-user-edit"></i> Edit Profile</a></li>
+    <li class= "nav-item"><a class="nav-link" href="#"><i class="fas fa-door-open"></i> Log Out</a></li>
+  </ul>
+</div>
+<div class="admin-content">
+  <div class="row m-0">
+<div class="sidebar-navigation bg-dark col-sm-2">
+  <ul class="nav pl-0">
+    <li class= "nav-item col-lg-12"><h4>Hello, User</h4></li>
+    <li class= "nav-item col-lg-12"><a class="nav-link" href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
+    <li class= "nav-item col-lg-12"><a class="nav-link" href="#"><i class="fas fa-file"></i> Posts</a></li>
+    <li class= "nav-item col-lg-12"><a class="nav-link" href="#"><i class="fas fa-book-open"></i> Pages</a></li>
+    <li class= "nav-item col-lg-12"><a class="nav-link" href="#"><i class="fas fa-list"></i> Categories</a></li>
+    <li class= "nav-item col-lg-12"><a class="nav-link" href="#"><i class="fas fa-box"></i> Products</a></li>
+    <li class= "nav-item col-lg-12"><a class="nav-link" href="#"><i class="fas fa-users"></i> Customers</a></li>
+    <li class= "nav-item col-lg-12"><a class="nav-link" href="#"><i class="fas fa-table"></i> Tables</a></li>
+    <li class= "nav-item col-lg-12"><a class="nav-link" href="#"><i class="fas fa-chart-line"></i> Charts</a></li>
+  </ul>
+</div>
+<div class="content col-sm-10 bg-secondary">
+
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+</body>
+
+</html>
